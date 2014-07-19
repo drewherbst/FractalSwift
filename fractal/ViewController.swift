fc03b78995477482287a3527efb1f9c742a62cff
aherbst <aherbst@smartertravelmedia.com>
2014-07-19 09:45:31 -0400

    Initial Commit

---
+++ fractal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fractal
+//
+//  Created by Andrew Herbst on 7/19/14.
+//  Copyright (c) 2014 drewherbst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
